faa4e21f2869d0d572add577acd427a2fc48e666
VishalDevelooperengineer <vk4993102@gmail.com>
2023-03-13 10:46:59 +0530

    add save posted

--- src/Components/activityPages/Saved.jsx
+++ src/Components/activityPages/Saved.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import token from '../../configs/authentication';
 import Profile from '../ProfilePages/Profile'
 
 const Saved = () => {
+  const userId = localStorage.getItem("Id")
+  const [post, setPost] = useState([]);
+  const getPost = () => {
+    axios({
+      method: "get",
+      url: "https://instagram-apis.onrender.com/instagram/v1/post/fetch",
+      headers: token,
+    }).then((res) => {
+      const data = res.data.posts;
+      const savedPost = data.filter((p)=>p.postSave.includes(userId))
+      setPost(savedPost);
+    }).catch((error)=>{
+      console.log(error.message)
+    });
+  };
+useEffect(() => {
+      getPost();
+}, []);
   return (
     <div>
         <Profile />
+        <section className="postsByuser">
+        <div className="post">
+          {post.map((item) => {
+            return (
+              <img
+                src={item.image}
+                key={item._id}
+                alt="post1"
+              />
+            );
+          })}
+        </div>
+      </section>
     </div>
   )
 }
